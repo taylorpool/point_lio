d8b65122f39e26979668c9474a879f9ccac84b4f
Taylor Pool <taylorpool.27@gmail.com>
2024-03-25 12:53:46 -0400

    Updated default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import ( builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz" ) {
 	overlays = [
 		( import ( ( builtins.fetchGit {
-			url = "git@github.com:lopsided98/nix-ros-overlay.git";
+			url = "https://github.com/lopsided98/nix-ros-overlay";
 			ref = "develop";
 			rev = "de1ea0a747635d6c0b33b7cfd99b75db6031d05d";
 		} ) + "/overlay.nix" ) )
