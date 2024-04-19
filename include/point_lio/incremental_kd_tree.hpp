2cc7fc9191d1cbcef8ba783b16fecfc83f423a17
Taylor Pool <taylorpool.27@gmail.com>
2024-04-19 15:20:07 -0400

    Stopping here for now

--- include/point_lio/incremental_kd_tree.hpp
+++ include/point_lio/incremental_kd_tree.hpp
@@ -2,54 +2,79 @@
 #define POINT_LIO_INCREMENTAL_KDTREE_HPP
 
 #include <Eigen/Dense>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <limits>
+#include <memory>
 #include <queue>
 #include <vector>
-#include <cmath>
-#include <limits>
-#include <functional>
-#include <deque>
 
 namespace point_lio {
-    const int k = 3;
+const int k = 3;
 const float alpha = 0.5f;
 
-struct Node
-{
-    Eigen::Vector3d point;
-    Node *left, *right;
+struct Node {
+  using Ptr = Node *;
+  Eigen::Vector3d point;
+  Ptr left;
+  Ptr right;
+
+  Node(Eigen::Vector3d point);
 };
 
 struct CompareDist {
-    bool operator()(std::pair<double, Eigen::Vector3d> const &a, std::pair<double, Eigen::Vector3d> const &b) {
-        return a.first < b.first;
-    }
+  bool operator()(std::pair<double, Eigen::Vector3d> const &a,
+                  std::pair<double, Eigen::Vector3d> const &b) {
+    return a.first < b.first;
+  }
 };
+
+Node::Ptr newNode(Eigen::Vector3d point);
+
+Node::Ptr insert(Eigen::Vector3d point);
+
+Node::Ptr insertRec(Node::Ptr root, Eigen::Vector3d point, unsigned depth);
+
+int treeSize(Node::Ptr root);
+
+int treeLeft(Node::Ptr root);
+
+int treeRight(Node::Ptr root);
+
+Node::Ptr build(const std::vector<Eigen::Vector3d> &v);
+
+bool arePointsSame(const Eigen::Vector3d point1, const Eigen::Vector3d point2);
 
 class KDTree {
 public:
+  KDTree();
 
-    KDTree();
+  Node::Ptr root;
 
-    Node* root;
-    std::deque<std::pair<Eigen::Vector3d, bool>> operationLogger;
+  std::deque<std::pair<Eigen::Vector3d, bool>> operationLogger;
 
-    Node* newNode(int arr[]);
-    Node* insertRec(Node* root, int point[], unsigned depth);
-    Node* insert(int point[]);
-    int treeSize(Node* root);
-    int treeLeft(Node* root);
-    int treeRight(Node* root);
-    Node* build(const std::vector<Eigen::Vector3d>& v);
-    void incrementalUpdates(Node* root, const std::pair<Eigen::Vector3d, bool>& operation, bool updateLogger);
-    Node* removeNode(Node* root, Eigen::Vector3d point, unsigned depth);
-    void parRebuild(Node *root);
-    void reBalance(Node *root, unsigned depth);
-    bool arePointsSame(const Eigen::Vector3d point1, const Eigen::Vector3d point2);
-    bool searchRec(Node* root, Eigen::Vector3d point, unsigned depth);
-    bool search(Node* root, Eigen::Vector3d point);
-    void findNearestNeighbors(Node* root, const Eigen::Vector3d& target, std::priority_queue<std::pair<double, Eigen::Vector3d>, std::vector<std::pair<double, Eigen::Vector3d>>, CompareDist>& pq, int depth, int k);
+  void incrementalUpdates(Node::Ptr root,
+                          const std::pair<Eigen::Vector3d, bool> &operation,
+                          bool updateLogger);
 
+  Node::Ptr removeNode(Node::Ptr root, Eigen::Vector3d point, unsigned depth);
+
+  void parRebuild(Node::Ptr root);
+
+  void reBalance(Node::Ptr root, unsigned depth);
+
+  bool searchRec(Node::Ptr root, Eigen::Vector3d point, unsigned depth);
+
+  bool search(Node::Ptr root, Eigen::Vector3d point);
+
+  void findNearestNeighbors(
+      Node::Ptr root, const Eigen::Vector3d &target,
+      std::priority_queue<std::pair<double, Eigen::Vector3d>,
+                          std::vector<std::pair<double, Eigen::Vector3d>>,
+                          CompareDist> &pq,
+      int depth, int k);
 };
-}
+} // namespace point_lio
 
 #endif
